fa9542c04ff8c35e886352bcdd8b2fca0e79b190
s111ew <167631312+s111ew@users.noreply.github.com>
2025-08-25 15:18:07 +0100

    feat: add Profile page skeleton

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,5 +1,30 @@
+import styles from "../../styles/Profile.module.css"
+import arrow from "../../assets/arrow_right_grey.svg"
+
 export default function Profile() {
   return(
-    <></>
+    <>
+      <div className={styles.profileContainer}>
+        <div className={styles.profile}>
+          <ul className={styles.optionList}>
+            <li className={styles.user}>
+              <div className={styles.userPic}></div>
+              <div className={styles.userNameContainer}>
+                <p className={styles.userName}>John Smith</p>
+                <p className={styles.userSince}>Member Since: <span>1 August 2025</span></p>
+              </div>
+            </li>
+            <div className={styles.divider}></div>
+            <li className={styles.optionItem}>About <img className={styles.arrowIcon} src={arrow} alt="Arrow icon" /></li>
+            <div className={styles.divider}></div>
+            <li className={styles.optionItem}>Benchmarking <img className={styles.arrowIcon} src={arrow} alt="Arrow icon" /></li>
+            <div className={styles.divider}></div>
+            <li className={styles.optionItem}>Settings <img className={styles.arrowIcon} src={arrow} alt="Arrow icon" /></li>
+            <div className={styles.divider}></div>
+            <li className={styles.optionItem}>Contact <img className={styles.arrowIcon} src={arrow} alt="Arrow icon" /></li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
